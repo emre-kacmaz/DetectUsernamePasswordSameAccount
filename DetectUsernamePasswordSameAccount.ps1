ab1cf674c52497175014fb00bc14480744d4a4cc
Emre Kaçmaz <muhemrekacmaz@gmail.com>
2023-03-23 11:10:58 +0300

    Update DetectUsernamePasswordSameAccount.ps1

--- DetectUsernamePasswordSameAccount.ps1
+++ DetectUsernamePasswordSameAccount.ps1
@@ -1,33 +1,18 @@
 Function Test-ADAuthentication {
-
     param(
 
         $username,
 
         $password)
 
- 
-
     (New-Object DirectoryServices.DirectoryEntry "",$username,$password).psbase.name -ne $null
-
 }
-
- 
-
- 
 
 $users = Get-ADUser -Filter * | select SamAccountName
 
- 
-
 foreach ($user in $users){
 
- 
-
 if ($(Test-ADAuthentication -username $user.SamAccountName -password ($user.SamAccountName).ToLower()) -eq "True"){
-
-Write-Host $user.SamAccountName}
-
- 
-
-} 
+    Write-Host $user.SamAccountName
+}
+    } 
