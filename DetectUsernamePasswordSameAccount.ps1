24ca6b9faccc9bef95542308820826d3de86fb3d
Emre Kaçmaz <muhemrekacmaz@gmail.com>
2023-03-23 11:09:36 +0300

    Add files via upload

---
+++ DetectUsernamePasswordSameAccount.ps1
@@ -0,0 +1,33 @@
+Function Test-ADAuthentication {
+
+    param(
+
+        $username,
+
+        $password)
+
+ 
+
+    (New-Object DirectoryServices.DirectoryEntry "",$username,$password).psbase.name -ne $null
+
+}
+
+ 
+
+ 
+
+$users = Get-ADUser -Filter * | select SamAccountName
+
+ 
+
+foreach ($user in $users){
+
+ 
+
+if ($(Test-ADAuthentication -username $user.SamAccountName -password ($user.SamAccountName).ToLower()) -eq "True"){
+
+Write-Host $user.SamAccountName}
+
+ 
+
+} 
